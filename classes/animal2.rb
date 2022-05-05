9eaf31734aed3585bb7d6c5dfb00f238a482f578
waffe_x20 <argueta_eduardo@yahoo.de>
2022-05-04 20:37:57 -0600

    Add factory pattern & class methods to animal2

--- classes/animal2.rb
+++ classes/animal2.rb
@@ -1,8 +1,19 @@
 class Animal
-    attr_accessor :noise
+
+    attr_accessor :noise, :color
 
     def initialize(options={})
         @noise = options[:noise] || 'Headless'
+        @color = options[:color] || 'white'
     end    
+
+    def self.types
+        ['pig', 'cow', 'conejo']
+    end
+
+    def self.create_a_pig
+        Animal.new({noise: 'Oink!', color: 'tempest'})
+    end
+
 end
     
--- init3.rb
+++ init3.rb
@@ -4,3 +4,14 @@ require_relative 'classes/animal2.rb'
 pig = Animal.new({noise: 'useless'})
 pig.noise = 'let'
 puts pig.noise
+
+puts "-" * 15
+
+Animal.types.each do |type|
+    puts type
+end
+
+# puts Animal.types.each {|type| type}
+
+wilbur = Animal.create_a_pig
+puts "#{wilbur.noise} / #{wilbur.color}"
